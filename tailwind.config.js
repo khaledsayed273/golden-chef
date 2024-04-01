6a04f05075f023e6da9fcb8bed2a133d53885929
khaled sayed <khaledsayed273@gmail.com>
2024-04-01 11:35:15 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        red: '#F54748',
+        yellow: "#FDC55E"
+      }
+    },
+  },
+  plugins: [],
+}
+
